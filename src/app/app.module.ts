a3618b440da265b91ec31338ad3486557474e5de
Andreia <andreia.machion@saojudas.br>
2021-04-27 23:56:22 -0300

    aula mean 01 atá listagem 2.7.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ClienteListaComponent } from './clientes/cliente-lista/cliente-lista.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ClienteInserirComponent, CabecalhoComponent, ClienteListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, NoopAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-cliente-inserir',
+  templateUrl: './cliente-inserir.component.html',
+  styleUrls: ['./cliente-inserir.component.css'],
+})
+
+export class ClienteInserirComponent {
+
+  nome: string;
+  fone: string;
+  email: string;
+
+  onAdicionarCliente() {
+    console.log('inserindo cliente...');
+  }
+
+}
