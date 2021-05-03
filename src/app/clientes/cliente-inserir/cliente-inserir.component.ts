6279341dff3186b860be6085c51981badb0fab0c
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-05-03 20:20:45 -0300

    implementação do cadastro com databinding

--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Cliente } from '../cliente.model';
 
-@Component ({
+@Component({
   selector: 'app-cliente-inserir',
   templateUrl: './cliente-inserir.component.html',
   styleUrls: ['./cliente-inserir.component.css'],
 })
-
 export class ClienteInserirComponent {
+  @Output() clienteAdicionado = new EventEmitter<Cliente>();
 
   nome: string;
   fone: string;
   email: string;
 
   onAdicionarCliente() {
-    console.log('inserindo cliente...');
+    const cliente: Cliente = {
+      nome: this.nome,
+      fone: this.fone,
+      email: this.email,
+    };
+    this.clienteAdicionado.emit(cliente);
   }
-
 }
--- src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cliente } from '../cliente.model';
 
 @Component({
   selector: 'app-cliente-lista',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-lista.component.css']
 })
 export class ClienteListaComponent implements OnInit {
+  @Input() clientes: Cliente[] = [];
 
   constructor() { }
 
