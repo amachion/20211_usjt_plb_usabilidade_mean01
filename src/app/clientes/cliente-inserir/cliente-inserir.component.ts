88b5a9641c5cb597537aeab91199047a832ce371
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-05-03 21:18:38 -0300

    Validação de erros no formulário

--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Cliente } from '../cliente.model';
 
 @Component({
@@ -13,11 +14,14 @@ export class ClienteInserirComponent {
   fone: string;
   email: string;
 
-  onAdicionarCliente() {
+  onAdicionarCliente(form: NgForm) {
+    if (form.invalid) {
+      return;
+      }
     const cliente: Cliente = {
-      nome: this.nome,
-      fone: this.fone,
-      email: this.email,
+      nome: form.value.nome,
+      fone: form.value.fone,
+      email: form.value.email,
     };
     this.clienteAdicionado.emit(cliente);
   }
