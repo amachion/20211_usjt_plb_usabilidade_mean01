da7bda75ac4572458caf53e3af85029cc6f31d5b
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-05-24 21:53:01 -0300

    commit apostila 18 full

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -3,14 +3,14 @@ import { Cliente } from './cliente.model';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { stringify } from '@angular/compiler/src/util';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class ClienteService {
   private clientes: Cliente[] = [];
   private listaClientesAtualizada = new Subject<Cliente[]>();
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
   getClientes():void {
     this.httpClient.get < {mensagem:string, clientes: any;} >('http://localhost:3000/api/clientes')
     .pipe(map((dados) => {
@@ -38,6 +38,7 @@ export class ClienteService {
         cliente.id = dados.id;
         this.clientes.push(cliente);
         this.listaClientesAtualizada.next([...this.clientes]);
+        this.router.navigate(['/'])
       }
     )
   }
@@ -55,8 +56,11 @@ export class ClienteService {
   }
 
   getCliente (idCliente: string) {
-    return {...this.clientes.find((cli) => cli.id === idCliente)};
+  //  return {...this.clientes.find((cli) => cli.id === idCliente)};
+  return this.httpClient.get<{_id: string, nome: string, fone: string, email:
+    string}>(`http://localhost:3000/api/clientes/${idCliente}`);
   }
+
 
   atualizarCliente (id: string, nome: string, fone: string, email:string) {
     const cliente: Cliente = { id, nome, fone, email};
@@ -69,6 +73,7 @@ export class ClienteService {
       this.clientes = copia;
       console.log(this.clientes);
       this.listaClientesAtualizada.next([...this.clientes]);
+      this.router.navigate(['/'])
     }));
   }
 
